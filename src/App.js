fa982d249af34693b6d45bb63855dbd308e51728
AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>
2021-06-14 00:08:11 +0300

    react state
    Signed-off-by: AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
+  let productInfo = { title: "Product List",baskaBisey:"bisey" };
+  let categoryInfo = { title: "Category List" };
   return (
+    //div bir container tag idir.
     <div>
-      <h2>Hello React Course</h2>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/Navi.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+// Class component
+// public --> export
+export default class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Navi Component</h2>
+            </div>
+        )
+    }
+}
